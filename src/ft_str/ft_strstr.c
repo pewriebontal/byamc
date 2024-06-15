27f38c47f1b1ec75e7d958a86016a039e35c24be
Pewrie Bontal <0x@bontal.net>
2024-06-15 12:51:18 +0800

    added strcat, strcpy, strstr

---
+++ src/ft_str/ft_strstr.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mkhaing <0x@bontal.net>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/14 06:52:02 by mkhaing           #+#    #+#             */
+/*   Updated: 2024/06/14 06:53:50 by mkhaing          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_strstr(const char *haystack, const char *needle)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	if (!*needle)
+		return ((char *)haystack);
+	while (haystack[i])
+	{
+		j = 0;
+		while (haystack[i + j] == needle[j])
+		{
+			if (!needle[j + 1])
+				return ((char *)&haystack[i]);
+			j++;
+		}
+		i++;
+	}
+	return (0);
+}
