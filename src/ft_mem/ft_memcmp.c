62af94a4fced12c9b5a3f0a2f70f075fa4ac00ab
Pewrie Bontal <0x@bontal.net>
2024-06-27 01:55:57 +0800

    Refactor ft_memcmp to use const unsigned char pointers

--- src/ft_mem/ft_memcmp.c
+++ src/ft_mem/ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: mkhaing <0x@bontal.net>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/16 11:31:42 by mkhaing           #+#    #+#             */
-/*   Updated: 2024/03/13 21:20:45 by mkhaing          ###   ########.fr       */
+/*   Updated: 2024/06/27 01:53:56 by mkhaing          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,9 @@
 
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-	size_t			i;
-	unsigned char	*p1;
-	unsigned char	*p2;
+	size_t				i;
+	const unsigned char	*p1;
+	const unsigned char	*p2;
 
 	i = 0;
 	p1 = (unsigned char *)s1;
